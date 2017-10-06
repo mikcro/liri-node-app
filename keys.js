e3ebd42d756cd76cffae2aaf4695c5489baba342
mikcro <nowitsforever@gmail.com>
2017-10-06 14:37:05 -0500

    first

---
+++ keys.js
@@ -0,0 +1,18 @@
+// console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: '9ZGtEnvUTCdvAqVYVgRHEHMvz',
+  consumer_secret: 'G9HE90AhUdsZPQcRnjk0tVNDqqMy20lv5VYRop21RSj2T3VGez',
+  access_token_key: '914941689625743362-vISVEkunFYPZJK9JbikucMd1L4eeKlU',
+  access_token_secret: 'Mil4tA3fomXVkp9mlPyGTL8IlJ0rv2NkWkWBoLVWlvmIQ',
+}
+
+exports.lastfmKeys = {
+  api_key: '8dd285f5c576b322e1341278b64ae1bd',
+  consumer_secret: 'e1bc5e25980654a4ef2f10d9b95ac83c',
+}
+
+exports.spotifyKeys = {
+  Client ID: '4ad15b0b8edf4a5e833bd1eedce71d1c',
+  Client Secret: 'c9d213fbdd4849eab5f619f19bfed2ca',
+}
---
+++ liri.js
@@ -0,0 +1,105 @@
+var keys = require("./keys.js");
+var Twitter = require('twitter');
+var lastFmNode = require('lastfm').LastFmNode;
+var request = require('request');
+
+// TWITTER//
+var client = new Twitter(keys.twitterKeys);
+
+var params = { screen_name: 'CroMikeDonMan' };
+client.get('statuses/user_timeline', params, function (error, tweets, response) {
+
+    if (error) {
+        return console.log("liri doesn't understand that commands");
+    }
+
+    if (!error) {
+        for (var i = 0; i < tweets.length; i<20) {
+            var date = tweets[i].created_at
+            var texts = tweets[i].text
+        }
+    }
+
+    if (process.argv[2] == "my-tweets") {
+        console.log(date, texts)
+    }
+
+
+});
+
+
+if(process.argv[2] == 'movie-this'){
+    movie(process.argv[3]);
+} else if (process.argv[2] == 'spotify-this-song'){
+    musicFm(process.argv[3]);
+}
+
+// SPOTIFY//
+
+
+function spotify(song) {
+var spotify = new lastFmNode(keys.spotifyKeys);
+    spotify.request("track.search", {
+        track: song, handlers: {
+            success: function (json) {
+
+                if (process.argv[2] == "spotify-this-song") {
+                    
+                    //   console.log(JSON.stringify(json, null, 2) + " LOL" )
+                    for (var i = 0; i < json.results.trackmatches.track.length; i++) {
+                    //   console.log(JSON.stringify(json, null, 2));
+                    console.log("Name: " + json.results.trackmatches.track[i].name)
+                    console.log("----------------------")
+                    console.log("Artist: " + json.results.trackmatches.track[i].artist)
+                    console.log("----------------------")
+                    console.log("Link: " + json.results.trackmatches.track[i].url)
+
+                    }
+
+                }
+
+
+            },
+            error: function (error) {
+                console.log("The Sign" + error.message);
+            }
+
+        }
+    });
+}
+
+//MOVIE THIS//
+
+
+function movie(movieName) {
+    
+    
+    request('http://www.omdbapi.com/?t=' +movieName + '&y=&plot=short&apikey=40e9cece', function (error, response, body) {
+        // console.log('error:', error); 
+        // console.log('statusCode:', response && response.statusCode); 
+        // console.log('body:', body); 
+
+        if(!error && response.statusCode == 200) {
+
+            var data = JSON.parse(body);
+
+            console.log("Title: " + data.Title);
+            console.log("Year: " + data.Year);
+            console.log("Rated: " + data.Rated);
+            console.log("Country: " + data.Country);
+            console.log("Actors: " + data.Actors);
+            console.log("Language: " + data.Language);
+            console.log("Plot: " + data.Plot);
+        }
+    })
+
+};
+
+
+
+
+
+
+
+
+
